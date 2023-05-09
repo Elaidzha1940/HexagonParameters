c2794b6aba55d634ba3607cd106be52936fd3a48
Elaidzha1940 <eli080293@gmail.com>
2023-05-09 12:35:20 +0700

    HexagonParameters + added CoreGraphics

---
+++ HexagonParameters/HexagonParameters/HexagonParameters.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: HexagonParameters
+//  File: HexagonParameters.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 09.05.2023
+//
+//  */
+
+import Foundation
+
+struct HexagonParameters {
+    
+    struct Segment {
+            let line: CGPoint
+            let curve: CGPoint
+            let control: CGPoint
+        }
+}
