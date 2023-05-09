6b243ecedbecfa5bfd730f3a39e7a26aa4ad63ce
Elaidzha1940 <eli080293@gmail.com>
2023-05-09 12:31:33 +0700

    HexagonParameters

---
+++ HexagonParameters/HexagonParameters/HexagonParametersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HexagonParametersApp.swift
+//  HexagonParameters
+//
+//  Created by Elaidzha Shchukin on 09.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct HexagonParametersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
